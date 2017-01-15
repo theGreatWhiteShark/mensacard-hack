80d2620c19d1abda554252e58e1068a0b9cdba34
shark <thetruephil@googlemail.com>
2017-01-15 22:52:31 +0100

    renaming the 'contact' tab to 'database'

--- ionic/src/app/app.module.ts
+++ ionic/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
+import { DatabasePage } from '../pages/database/database';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
@@ -10,7 +10,7 @@ import { TabsPage } from '../pages/tabs/tabs';
   declarations: [
     MyApp,
     AboutPage,
-    ContactPage,
+    DatabasePage,
     HomePage,
     TabsPage
   ],
@@ -21,7 +21,7 @@ import { TabsPage } from '../pages/tabs/tabs';
   entryComponents: [
     MyApp,
     AboutPage,
-    ContactPage,
+    DatabasePage,
     HomePage,
     TabsPage
   ],
