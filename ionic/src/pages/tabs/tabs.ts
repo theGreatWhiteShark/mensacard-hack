908175061607ab5110745f5da4e98dc0f6feb111
shark <thetruephil@googlemail.com>
2017-01-15 22:51:53 +0100

    minor tweaks

--- ionic/src/pages/tabs/tabs.ts
+++ ionic/src/pages/tabs/tabs.ts
@@ -2,19 +2,22 @@ import { Component } from '@angular/core';
 
 import { HomePage } from '../home/home';
 import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
+import { DatabasePage } from '../database/database';
 
 @Component({
-  templateUrl: 'tabs.html'
+		templateUrl: 'tabs.html'
 })
 export class TabsPage {
-  // this tells the tabs component which Pages
-  // should be each tab's root Page
-  tab1Root: any = HomePage;
-  tab2Root: any = AboutPage;
-  tab3Root: any = ContactPage;
+		// this tells the tabs component which Pages
+		// should be each tab's root Page
+    
 
-  constructor() {
 
-  }
+		tab1Root: any = HomePage;
+		tab2Root: any = DatabasePage;
+		tab3Root: any = AboutPage;
+
+		constructor() {
+
+		}
 }
