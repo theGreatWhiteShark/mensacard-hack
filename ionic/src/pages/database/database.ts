78c231630cb6e3b48eb0a9614cce84c0909d43f0
shark <thetruephil@googlemail.com>
2017-01-22 22:45:20 +0100

    mock data, list of all data and selected Since deploying the actual NFC plugin will be a somewhat final step I introduced some mock data. In addition I set up a simple list displaying all the data in the mock data array and a short section displaying information about the entry selected from the list

--- ionic/src/pages/database/database.ts
+++ ionic/src/pages/database/database.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+// import { Storage } from '@ionic/storage';
 
 import { NavController } from 'ionic-angular';
 
@@ -8,8 +9,32 @@ import { NavController } from 'ionic-angular';
 })
 export class DatabasePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController
+	      //storage: Storage
+  ) {
 
   }
+  // porperty to bind to
+  cards = MockCards;
 
+  // just for fun
+  selectedCard: Mensacard;
+  onSelect( card: Mensacard ): void { this.selectedCard = card; }
+  
+    
 }
+// Defining the Mensacard object. At some point this will
+// be replaced by the read NFC content
+export class Mensacard {
+  name: string;
+  id: number;
+}
+
+// creating a bunch of placeholder cards to fill the
+// database for testing
+const MockCards: Mensacard[] = [
+  { name: 'Lonnie', id: 2231 },
+  { name: 'John Doo', id: 932 },
+  { name: 'Charles', id: 3 },
+  { name: 'Ines', id: 36923 },
+  { name: 'Caja', id: 1 } ];
